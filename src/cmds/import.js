90ec6edfd07983d304ef092252e223ebaa792401
hsaginor <hsaginor@gmail.com>
2023-08-18 12:09:51 -0700

    adding better error handling if url fetch fails

--- src/cmds/import.js
+++ src/cmds/import.js
@@ -136,8 +136,8 @@ const buildReport = async (importStatus) => {
     return workbook.xlsx.writeBuffer();
 };
 
-const saveReport = async (targetDir, importStatus) => {
-    const reportFilePath = Path.join(targetDir,'import-report.xlsx');
+const saveReport = async (importStatus) => {
+    const reportFilePath = Path.join(importStatus.targetDir,'import-report.xlsx');
     const blob = await buildReport(importStatus);
     await saveFile(reportFilePath, blob);
 };
@@ -151,6 +151,75 @@ const htmlTo = async (url, doc, projectTransformer, config, params) => {
     return await html2md(url, doc, projectTransformer, config, params);
 }
 
+const updateTimer = (importStatus) => {
+    const totalTime = Math.round((new Date() - importStatus.startTime) / 1000);
+    let timeStr = `${totalTime}s`;
+    if (totalTime > 60) {
+      timeStr = `${Math.round(totalTime / 60)}m ${totalTime % 60}s`;
+      if (totalTime > 3600) {
+        timeStr = `${Math.round(totalTime / 3600)}h ${Math.round((totalTime % 3600) / 60)}m`;
+      }
+    }
+    importStatus.timeStr = timeStr;
+}
+
+const processUrl = async (url, importStatus) => {
+    let outputTypes = importStatus.outputTypes;
+    let targetDir = importStatus.targetDir;
+    const resp = await fetch(url);
+    if (resp.ok) {
+        if (resp.redirected) {
+            console.log(`${importStatus.imported}/${importStatus.total}. ${url} redirected`);
+            importStatus.rows.push({
+                url,
+                status: 'Redirect',
+                redirect: resp.url
+            });
+        } else {
+            try {
+                const text = await resp.text();
+                const doc = new JSDOM(text).window.document;
+                fixLinks(url, doc, ['srcset', 'src']);
+                // console.log(`${importStatus.imported}/${importStatus.total}. Processing ${url}`);
+                const config = { toMd: outputTypes.includes('md'), toDocx: outputTypes.includes('docx') };
+                const result = await htmlTo(url, doc, importStatus.projectTransformer, config, importStatus.params);
+                const path = WebImporter.FileUtils.sanitizePath(result.path);
+                const docPath = `${documentPath(targetDir, path)}`;
+                const files = await saveOutput(docPath, outputTypes, result);
+                updateTimer(importStatus);
+                console.log(`${importStatus.imported}/${importStatus.total}. Imported ${url}. Elapsed time: ${importStatus.timeStr}`);
+                const report = (result.report) ? result.report : {};
+                Object.keys(report).forEach((key) => {
+                    if (!importStatus.extraCols.includes(key)) {
+                      importStatus.extraCols.push(key);
+                    }
+                });
+                for (let file of files) {
+                    importStatus.rows.push({
+                        url,
+                        status: 'Success',
+                        path: docPath,
+                        file,
+                        report
+                    });
+                }
+            } catch(error) {
+                console.error(error);
+                importStatus.rows.push({
+                    url,
+                    status: `Error: ${error.message}`
+                });
+            }
+        }
+    } else {
+        console.log(`${importStatus.imported}/${importStatus.total}. ${url} return status ${resp.status}`);
+        importStatus.rows.push({
+            url,
+            status: `Invalid: ${resp.status}`,
+        });
+    }
+}
+
 const handler = async (argv) => {
     const importStatus = {
         imported: 0,
@@ -161,70 +230,30 @@ const handler = async (argv) => {
 
     const outputTypes = argv.type.split('|');
     validateOutputType(outputTypes);
-    const targetDir = absPath(argv.target);
-    fs.ensureDir(targetDir);
+    importStatus.outputTypes = outputTypes;
+    importStatus.targetDir = absPath(argv.target);
+    fs.ensureDir(importStatus.targetDir);
     const tsPath = absPath(argv.transformScript);
-    const projectTransformer = await import(tsPath);
+    importStatus.projectTransformer = await import(tsPath);
     const entries = await getEntries(argv.urls);
-    const params = (argv.params) ? await asJson(argv.params) : {};
+    importStatus.params = (argv.params) ? await asJson(argv.params) : {};
     importStatus.total = entries.length;
+    importStatus.startTime = Date.now();
     await Utils.asyncForEach(entries, async (url, index) => {
         importStatus.imported += 1;
-        const resp = await fetch(url);
-        if (resp.ok) {
-            if (resp.redirected) {
-                console.log(`${importStatus.imported}/${importStatus.total}. ${url} redirected`);
-                importStatus.rows.push({
-                    url,
-                    status: 'Redirect',
-                    redirect: resp.url
-                });
-            } else {
-                try {
-                    const text = await resp.text();
-                    const doc = new JSDOM(text).window.document;
-                    fixLinks(url, doc, ['srcset', 'src']);
-                    // console.log(`${importStatus.imported}/${importStatus.total}. Processing ${url}`);
-                    const config = { toMd: outputTypes.includes('md'), toDocx: outputTypes.includes('docx') };
-                    const result = await htmlTo(url, doc, projectTransformer, config, params);
-                    const path = WebImporter.FileUtils.sanitizePath(result.path);
-                    const docPath = `${documentPath(targetDir, path)}`;
-                    const files = await saveOutput(docPath, outputTypes, result);
-                    console.log(`${importStatus.imported}/${importStatus.total}. ${url} ok`);
-                    const report = (result.report) ? result.report : {};
-                    Object.keys(report).forEach((key) => {
-                        if (!importStatus.extraCols.includes(key)) {
-                          importStatus.extraCols.push(key);
-                        }
-                    });
-                    for (let file of files) {
-                        importStatus.rows.push({
-                            url,
-                            status: 'Success',
-                            path: docPath,
-                            file,
-                            report
-                        });
-                    }
-                } catch(error) {
-                    console.error(error);
-                    importStatus.rows.push({
-                        url,
-                        status: `Error: ${error.message}`
-                    });
-                }
-            }
-        } else {
-            console.log(`${importStatus.imported}/${importStatus.total}. ${url} return status ${resp.status}`);
+        try {
+            await processUrl(url, importStatus)
+        } catch(error) {
+            console.error(error);
             importStatus.rows.push({
                 url,
-                status: `Invalid: ${resp.status}`,
+                status: `Error: ${error.message}`
             });
         }
     });
 
     console.log('Done! Saving report.');
-    await saveReport(targetDir, importStatus);
+    await saveReport(importStatus);
 };
 
 export default {
